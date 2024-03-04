fb3c160678b6598f69e4d11b8f915cce13a4a862
YadhuCU <yadhuakku003@gmail.com>
2024-03-04 09:59:14 +0530

    initial commit completed basic design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllProductsComponent } from './components/all-products/all-products.component';
+import { ViewProductComponent } from './components/view-product/view-product.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { WishlistComponent } from './components/wishlist/wishlist.component';
+import { UserCartComponent } from './components/user-cart/user-cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AllProductsComponent },
+  { path: 'view/:id', component: ViewProductComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'wishlist', component: WishlistComponent },
+  { path: 'cart', component: UserCartComponent },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { AllProductsComponent } from './components/all-products/all-products.component';
+import { ViewProductComponent } from './components/view-product/view-product.component';
+import { UserCartComponent } from './components/user-cart/user-cart.component';
+import { WishlistComponent } from './components/wishlist/wishlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    RegisterComponent,
+    AllProductsComponent,
+    ViewProductComponent,
+    UserCartComponent,
+    WishlistComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
